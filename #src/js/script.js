9760d97c85775322da9ac8ddef7ce7b044313f54
codelife404 <ihtiylor.dj33@gmail.com>
2020-11-21 19:18:51 +0500

    first commit

---
+++ #src/js/script.js
@@ -0,0 +1,10 @@
+const { images } = require("../../gulpfile");
+
+// @@include('alert.js')
+// function testWebp(callback) {
+//     var webP =new images();
+//     webP.onload=webP.onerror =function(){
+//         callback(webP.height==2);
+//     };
+//     webP.src=
+// }
---
+++ dist/js/script.js
@@ -0,0 +1,1 @@
+const{images:images}=require("../../gulpfile");
