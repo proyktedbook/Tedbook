76a2e6f36614a8d47f6567c5a532f9a44a6d7160
codelife404 <ihtiylor.dj33@gmail.com>
2020-11-26 14:31:35 +0500

    video joyi ham tayor

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 $(document).ready((function(){$("#phone").mask("+(999) 99-999-99-99")}));
-=======
-const{images:images}=require("../../gulpfile");
->>>>>>> 0d8dbd2282fb457656272ac622009b2f7085555d
